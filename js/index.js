e9ee0f689f674f390acadbfebc1afcb79c1d049b
Amos Gichero <amosgichero@gmail.com>
2020-10-30 15:55:18 -0400

    customize float button

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
+const floatbutton = document.getElementById("btnFloat");
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (
+		document.body.scrollTop > 150 ||
+		document.documentElement.scrollTop > 150
+	) {
+		floatbutton.style.display = "block";
+	} else {
+		floatbutton.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
 
 navToggle.addEventListener("click", () => {
 	document.body.classList.toggle("nav-open");
